de394ef092862de9baf3f1d6a2b1d49f4f00b64e
ICEBergJQ <salahsk52@gmail.com>
2025-05-07 13:47:44 +0100

    done hh

--- js/login.js
+++ js/login.js
@@ -20,25 +20,40 @@ export function renderLoginSec() {
 
 function addLoginEvent() {
     const button = document.getElementById('loginbtn')
+    const username = document.getElementById('username');
+    const password = document.getElementById('password');
 
-    button.addEventListener('click', async () => {
-        const username = document.getElementById('username').value;
-        const password = document.getElementById('password').value;
 
-        const response = await fetch('https://learn.zone01oujda.ma/api/auth/signin', {
-            method: 'POST',
-            headers: {
-                'Authorization': `Basic ${btoa(`${username}:${password}`)}`,
-                'Content-Type': 'application/json',
-            },
-        });
-
-        if (response.ok) {
-            const data = await response.json();
-            localStorage.setItem('jwt', data);
-            renderProfile();
-        } else {
-            displayToast("red", 'invalid credentials!')
+    button.addEventListener('click', login());
+    username.addEventListener('keypress', function (e) {
+        if (e.key === 'Enter') {
+          login();
         }
     });
+    password.addEventListener('keypress', function (e) {
+        if (e.key === 'Enter') {
+          login();
+        }
+    });
+}
+
+async function  login() {
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    const response = await fetch('https://learn.zone01oujda.ma/api/auth/signin', {
+        method: 'POST',
+        headers: {
+            'Authorization': `Basic ${btoa(`${username}:${password}`)}`,
+            'Content-Type': 'application/json',
+        },
+    });
+
+    if (response.ok) {
+        const data = await response.json();
+        localStorage.setItem('jwt', data);
+        renderProfile();
+    } else {
+        displayToast("red", 'invalid credentials!')
+    }
 }
